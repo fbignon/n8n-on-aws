7788d0fe8a5f37ce399267d2761e5d79daa7707e
fbignon <fabianobignon@gmail.com>
2025-05-13 18:06:48 -0300

    Atualizacao modificar o output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,3 @@
-output "instance_name" {
-  value = local.instance_name
-}
-
-output "security_group_name" {
-  value = local.security_group_name
-}
-
 output "n8n_url" {
   value = "http://${aws_instance.n8n.public_ip}:5678"
 }
