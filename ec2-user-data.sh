530fc819093807d5a96926b9128173a15d8754de
fbignon <fabianobignon@gmail.com>
2025-05-13 15:51:47 -0300

    Atualiza ec2-user-data.sh para Ubuntu

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-yum update -y
-amazon-linux-extras install docker -y
-service docker start
-usermod -a -G docker ec2-user
-chkconfig docker on
 
-# Instala Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
-  -o /usr/local/bin/docker-compose
+# Atualiza pacotes e instala Docker
+apt-get update -y
+apt-get install -y docker.io git curl
+
+# Inicia e habilita o serviço Docker
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ubuntu
+
+# Instala Docker Compose (v2 mais recente)
+curl -SL https://github.com/docker/compose/releases/download/v2.24.7/docker-compose-linux-x86_64 \
+    -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Clona e inicia o n8n
-cd /home/ec2-user
-yum install git -y
-git clone https://github.com/fbignon/n8n-on-aws.git
+# Clona repositório com docker-compose.yml (ou use cp se já copiou via Terraform)
+cd /home/ubuntu
+git clone https://github.com/SEU_USUARIO/n8n-on-aws.git
 cd n8n-on-aws
-docker-compose up -d
+chown -R ubuntu:ubuntu /home/ubuntu/n8n-on-aws
+
+# Sobe o serviço n8n
+sudo -u ubuntu docker-compose up -d
