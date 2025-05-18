cb05f4b9907362887d8da2ddac6fced876a549d1
fbignon <fabianobignon@gmail.com>
2025-05-17 22:59:45 -0300

    Atualizacao ec2.sh para parar o nginx do SO novamente

--- ec2-user-data-final-with-restore.sh
+++ ec2-user-data-final-with-restore.sh
@@ -17,7 +17,7 @@ systemctl enable docker
 usermod -aG docker ubuntu
 # Instalar docker-compose (versão compatível com docker 20.10+)
 #sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 
 
